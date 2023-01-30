2c122ba0655f1a4fd4aa7178ed48fa1c4dfb7f8e
Swordzi <github@sword.mozmail.com>
2023-01-30 12:20:08 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+
+<script>
+	import { Button } from '@svelteuidev/core';
+	import '../app.css';
+</script>
+
+<slot />
+
+<ul class="list-none m-0 p-0 overflow-hidden bg-background">
+	<li class="float-left py-5 px-5">
+		<Button class="bg-transparent">Hello</Button>
+	</li>
+	<li class="float-left bg-red-200 ">
+	
+	</li>
+</ul>
+
+<h1 class="text-red-900 text-3xl">Welcome to SvelteKit</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to r ead the documentation</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'background': '#181A1B',
+			'blue': '#2E6575'
+		}
+	},
+	plugins: []
+};
