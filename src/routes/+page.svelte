e46a8be73e19ee381176e796a9a916f858f37141
Swordzi <github@sword.mozmail.com>
2023-01-31 18:04:56 +0200

    working on navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,43 @@
-
-
 <script>
-	import { Button } from '@svelteuidev/core';
+	import { onMount } from 'svelte';
 	import '../app.css';
+	let mobileView = false;
+	const navItems = [
+		{ label: 'logo', href: '#' },
+		{ label: 'About', href: '#' },
+		{ label: 'Projects', href: '#' },
+		{ label: 'Repositories', href: '#' },
+		{ label: 'toggletheme', href: '#' }
+	];
+	const mobileHandler = () => (mobileView = !mobileView);
+	const queryHandler = (/** @type {{ matches: any; }} */ e) => {
+		if (!e.matches) {
+			mobileView = false;
+		}
+	};
+	onMount(() => {
+		const mediaListener = window.matchMedia('(max-width: 767px)');
+		mediaListener.addListener(queryHandler);
+	});
 </script>
 
 <slot />
 
-<ul class="list-none m-0 p-0 overflow-hidden bg-background">
-	<li class="float-left py-5 px-5">
-		<Button class="bg-transparent">Hello</Button>
-	</li>
-	<li class="float-left bg-red-200 ">
-	
-	</li>
-</ul>
+<nav class="bg-background font-sans h-45">
+	<div class="max-w-980 px-20 m-auto box-border flex items-center h-full">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div on:click={mobileHandler} class="w-6 h-4 relative cursor-pointer">
+			<div class="content-none absolute w-full h-0.5 bg-background transition-all duration-75 transform origin-center" />
+		</div>
+		<ul class={`navbar-list${mobileView ? ' mobile' : ''}`}>
+			{#each navItems as item}
+				<li>
+					<a href={item.href}>{item.label}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</nav>
 
 <h1 class="text-red-900 text-3xl">Welcome to SvelteKit</h1>
 
